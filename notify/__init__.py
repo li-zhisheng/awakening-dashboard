dc1d482cca33de3b58cb646d4a6452088abfd13a
李智晟 <2093782305@qq.com>
2026-09-19 18:58:24 +0800

    publish dashboard snapshot

--- notify/__init__.py
+++
@@ -1,4 +0,0 @@
-"""notify包: 告警手机推送。"""
-from notify.notifier import Notifier
-
-__all__ = ["Notifier"]
--- 人工买入校验.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem 人工买入对账按钮: 人工买入后双击, 校验当日成交并确认同步系统持仓
-cd /d %~dp0
-python main.py manual_audit --side buy
-pause
